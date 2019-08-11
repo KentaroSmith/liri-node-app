11858f0d24f445458ae01a2543a6f28c99a43801
Dan Smith <daniel.smith8795@gmail.com>
2019-08-11 12:14:04 -0500

    Fixed the do-what-it-says and omdb axios call

--- liri.js
+++ liri.js
@@ -61,13 +61,13 @@ var songSearch = function (song) {
     }
     console.log("Here is some information about songs named: " + song);
     console.log("====================================================");
-    console.log("Full result: "+result.tracks.items[0].album.name)
+    console.log("Full result: " + result.tracks.items[0].album.name)
 
     for (var i = 0; i < result.tracks.items.length; i++) {
       //var log = result.tracks.items;
       //fs.appendFile("log.txt", log)
-           console.log("Song Name: "+result.tracks.items[i].name)
-      console.log("Artist Name: "+result.tracks.items[i].artists[0].name)
+      console.log("Song Name: " + result.tracks.items[i].name)
+      console.log("Artist Name: " + result.tracks.items[i].artists[0].name)
       console.log("Album Name: " + result.tracks.items[i].album.name);
       console.log("Listen: " + result.tracks.items[i].external_urls.spotify);
       console.log("====================================================");
@@ -76,24 +76,39 @@ var songSearch = function (song) {
   });
 };
 
-var movieSearch= function(title){
-
-axios
-.get("http://www.omdbapi.com/?apikey=trilogy&"+title)
-.then(function(response){
-    console.log("Here is some information about songs named: " + title);
-    console.log("====================================================");
-    //Left Off trying to interpret JSON file
-    console.log(response)
-    console.log("Movie Title: ");
-    console.log("Release Year: ");
-    console.log("IMDB Rating: ");
-    console.log("Rotten Tomatoes Rating: ");
-    console.log("Production Country: ");
-    console.log("Language: ");
-    console.log("Plot: ");
-    console.log("Cast: ");
-})
+var movieSearch = function (title) {
+  if (!title) {
+    title = "Mr. Nobody";
+  }
+  axios
+    .get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy")
+    .then(function (response) {
+      console.log("Here is some information about the movie: " + title);
+      console.log("====================================================");
+      console.log("Movie Title: " + response.data.Title);
+      console.log("Release Year: " + response.data.Year);
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+      console.log("Production Country: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Cast: " + response.data.Actors);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
 }
 
 
@@ -108,23 +123,27 @@ switch (process.argv[2]) {
   case ('movie-this'):
     movieSearch(searchString);
     break;
-  //Spotify API
+  //using fs to read files
   case ('do-what-it-says'):
-    console.log("Read txt file");
     fs.readFile("random.txt", "utf8", function (error, data) {
       if (error) {
         return console.log(error)
       }
       var text = data.split(",");
       var command = text[0];
-      var song = text[1]
-      spotify.search({ type: 'track', query: song }, function (err, result) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
+      var searchTerm = text[1]
+      switch (command) {
+        case ('concert-this'):
+          concertSearch(searchTerm);
+          break;
+        case ('spotify-this-song'):
+          songSearch(searchTerm);
+          break;
+        case ('movie-this'):
+          movieSearch(searchTerm);
+          break;
 
-        console.log(result.tracks);
-      });
+      }
     })
     break;
   //unrecognized commands
